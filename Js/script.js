37d91a775d11582544fcf19cff2ff60180dbcd81
007hyno <Parasrawat05@gmail.com>
2023-08-28 16:30:07 +0530

    Added refine features and glass efect

--- Js/script.js
+++ Js/script.js
@@ -3,51 +3,70 @@ document.addEventListener('DOMContentLoaded', function() {
   const section2 = document.querySelector(".sec2");
   const contact = document.querySelector(".contact");
   const footer = document.querySelector("footer");
-  const home = document.querySelector(".home");
-  const section = document.querySelector(".sec1");
+  // const navHomeBtn = document.querySelector(".home");
+  // const section = document.querySelector(".sec1");
   
+  const service1 = document.querySelector(".si1");
+  const service2 = document.querySelector(".si2");
+  const service3 = document.querySelector(".si3");
+  
+  const cb = document.querySelector(".copyBtn");
+  
+  const homeLink = document.querySelector("#homeLink");
+  const aboutLink = document.querySelector("#aboutLink");
+  const servicesLink = document.querySelector("#servicesLink");
 
+  const home = document.querySelector("#home");
+  const services = document.querySelector("#services");
+  const about = document.querySelector("#about");
+  
 window.addEventListener('scroll', () => {
   var scroll= window.scrollY;
   const logo = document.querySelector(".navLogo");
   const banner2 = document.querySelector(".banner2");
   const banner3 = document.querySelector(".banner3");
   
-  // document.body.style.setProperty('--scroll', scroll);
   document.body.style.setProperty('--scroll', scroll+ 'px');
 
   console.log(scroll);
 if(scroll>100){
    logo.classList.add('rotate');
    logo.classList.remove('pRotate')
-  }if(scroll>390){
-    // banner2.classList.add('bottomUp');
-    
-  }
-  else{
-    
-    logo.classList.remove('rotate');
-    logo.classList.add('pRotate');
-  }
-  if(scroll>1200){
-    // banner2.classList.remove('bottomUp');
   }
   
   if (isElementInViewport(banner2)) {
-    console.log('Element is visible.');
     banner2.classList.add('bottomUp');
-
   }
   
   if (isElementInViewport(banner3)) {
-    console.log('Element is visible.');
     banner3.classList.add('bottomUp');
-
-  } else {
-    console.log('Element is not visible.');
-    // banner2.classList.remove('bottomUp');
   }
+  
+  //Nav Active
 
+  if (isElementInViewport(home)) {
+    homeLink.classList.add('active');
+    servicesLink.classList.remove('active');
+    aboutLink.classList.remove('active');
+  }
+  if (isElementInViewport(about)) {
+    homeLink.classList.remove('active');
+    aboutLink.classList.add('active');
+    servicesLink.classList.remove('active');
+  }
+  if (isElementInViewport(services)) {
+    homeLink.classList.remove('active');
+    aboutLink.classList.remove('active');
+    servicesLink.classList.add('active');
+  }
+  //Nav Active
+  
+  // navBlur{
+    // if(scroll>30){
+
+    // }
+  
+  // navBlur{
 }, false);
 
 
@@ -58,10 +77,10 @@ learnMore.addEventListener('click', e=> {
     block: "start"
   });
   });
-home.addEventListener('click', e=> {
-  e.preventDefault();
-  window.scrollTo(0, 0);
-  });
+// navHomeBtn.addEventListener('click', e=> {
+//   // e.preventDefault();
+//   window.scrollTo(0, 0);
+//   });
 contact.addEventListener('click', e=> {
   e.preventDefault();
   footer.scrollIntoView({
@@ -69,6 +88,45 @@ contact.addEventListener('click', e=> {
     block: "start"
   });
   });
+
+  learnMore.addEventListener('click', e=> {
+    e.preventDefault();
+    });
+
+  // Copy Email Address
+  cb.addEventListener('click', ()=> {
+    navigator.clipboard.writeText("CreatorDesk247@gmail.com");
+    cb.textContent = 'Copied';
+    cb.classList.add("copyBtn2")
+    cb.classList.remove("copyBtn")
+    setTimeout(() => {
+      cb.textContent = 'Copy';
+      cb.classList.remove("copyBtn2")
+      cb.classList.add("copyBtn")
+    }, 1500)
+  });
+
+
+
+  // Serice Highliter
+  service1.addEventListener('click', ()=> {
+    service1.classList.add("serviceH")    
+    service2.classList.remove("serviceH")    
+    service3.classList.remove("serviceH")    
+
+
+  });
+  service2.addEventListener('click', ()=> {
+    service1.classList.remove("serviceH")    
+    service2.classList.add("serviceH")    
+    service3.classList.remove("serviceH")    
+  });
+  service3.addEventListener('click', ()=> {
+    service1.classList.remove("serviceH")    
+    service2.classList.remove("serviceH")    
+    service3.classList.add("serviceH")    
+  });
+  // Serice Highliter
 
 
 
@@ -95,10 +153,9 @@ t1.addEventListener('click', ()=> {
   t5.addEventListener('click', function() {
     window.open('https://www.youtube.com/@MohitBisht','_blank') 
   });  
-  
   // User Testimonial click handler
   
-
+  
   function isElementInViewport(el) {
     const rect = el.getBoundingClientRect();
     return (
@@ -108,10 +165,6 @@ t1.addEventListener('click', ()=> {
         rect.right <= (window.innerWidth || document.documentElement.clientWidth)
     );
   }
-
-
-
-
 
 
       });
